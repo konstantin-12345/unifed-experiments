b3d93f0fa987b8d4629b499a995a104752daa400
konstantin <168954900+konstantin-12345@users.noreply.github.com>
2025-04-28 18:47:42 +0800

    Update unifed_server.py

--- unifed_server.py
+++ unifed_server.py
@@ -1,16 +1,16 @@
-# unifed_server.py
+# File: unifed_server.py
 import argparse
 import flwr as fl
 from strategy import UniFedStrategy
 
-def main():
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--rounds", type=int, default=400)
     parser.add_argument("--log-dir", type=str, default="./logs")
     args = parser.parse_args()
 
     strategy = UniFedStrategy(
-        fraction_fit=0.1,  # 选取比例
+        fraction_fit=0.1,
         min_fit_clients=10,
         min_available_clients=10,
     )
@@ -20,6 +20,3 @@ def main():
         config=fl.server.ServerConfig(num_rounds=args.rounds),
         strategy=strategy,
     )
-
-if __name__ == "__main__":
-    main()
